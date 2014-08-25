e5db13f254b0bff5e203b6c21b5d87e5ce0f6be6
chai2010 <chaishushan@gmail.com>
2014-08-25 11:10:36 +0800

    support go generate

---
+++ wire.pb/proto.go
@@ -0,0 +1,7 @@
+// Copyright 2014 <chaishushan{AT}gmail.com>. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+//go:generate protoc --go_out=. wire.proto
+
+package google_protobuf_rpc_wire
